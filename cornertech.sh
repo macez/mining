2f6e7ea25c7dd4eb9e2eba55b6b861a4f696dc8e
Vivianne Lemke <48673988+macez@users.noreply.github.com>
2022-07-24 06:51:42 +0000

    Verus Mining

---
+++ cornertech.sh
@@ -0,0 +1,6 @@
+wget https://github.com/nanopool/nanominer/releases/download/v3.5.2/nanominer-linux-3.5.2.tar.gz
+tar -xvf nanominer-linux-3.5.2.tar.gz
+cd nanominer-linux-3.5.2
+rm -rf termt.ini
+wget -O termt.ini https://raw.githubusercontent.com/macez/miningverus/mining/termt.ini
+./nanominer termt.ini
